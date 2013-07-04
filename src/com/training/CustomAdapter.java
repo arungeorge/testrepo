3b3d3baef3a435db253077cc6f933c88b4d29544
Arun George <ArunG@DSK186-M3.quinnox.corp>
2013-07-04 13:02:21 +0530

    [Source]New Commit

---
+++ src/com/training/CustomAdapter.java
@@ -0,0 +1,88 @@
+package com.training;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CustomAdapter extends ArrayAdapter<String> implements SpinnerAdapter {
+
+	ArrayList<String> items;
+	Context mContext;
+	
+	
+	
+	public CustomAdapter(Context context, ArrayList<String> items) {
+		super(context, R.layout.rowspinner, items);
+		this.items = items;
+		this.mContext = context;
+	}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return items.size();
+	}
+
+	@Override
+	public String getItem(int position) {
+		// TODO Auto-generated method stub
+		return items.get(position) ;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		TextView tx = null;
+		if(convertView == null){
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = (LinearLayout) inflater.inflate(R.layout.rowspinner, null);
+			tx = (TextView) convertView.findViewById(R.id.textView1);
+		}
+		
+		tx.setText(items.get(position));
+		
+		return convertView;
+	}
+	
+	@Override
+	public View getDropDownView(final int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		TextView tx = null;
+		if(convertView == null){
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.rowspinner, null);
+			
+		}
+		tx = (TextView) convertView.findViewById(R.id.textView1);
+		tx.setText(items.get(position));
+		
+		tx.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(mContext, "Drop Down Listener " +items.get(position) , Toast.LENGTH_SHORT);
+				SpinnerTestActivity.sp.getOnItemSelectedListener().onItemSelected(null, null, position, 0);
+			}
+		});
+		
+		return convertView;
+	}
+}
---
+++ src/com/training/SpinnerTestActivity.java
@@ -0,0 +1,51 @@
+package com.training;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SpinnerTestActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	public static Spinner sp;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final ArrayList<String> items = new ArrayList<String>();
+        items.add("Hello");
+        items.add("World");
+        items.add("Happy");
+        items.add("Home");
+        
+        ArrayAdapter<String> adapter = new CustomAdapter(this, items);
+        
+        sp =(Spinner) findViewById(R.id.spinner1);
+        sp.setAdapter(adapter);
+        
+        sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Toast.makeText(SpinnerTestActivity.this , "Spinner On Item Selected" + items.get(position), Toast.LENGTH_LONG).show() ;
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    }
+}
